3bc7b325551df0afbf976c38dc61e2fa9db165d5
Mark Probst <mark.probst@gmail.com>
2016-02-27 12:59:40 -0800

    Code formatting

--- cube4.4.py
+++ cube4.4.py
@@ -1,130 +1,142 @@
-import time #this is a Python library
+import time  # this is a Python library
 
-CUBE_SIZE=4   #length of edge of cube
-J3={1,3,5,7,9,10,11,12,14,16,17,18,20,21,23,24,25}
-J4={1,4,5,8,9,10,11,12,14,15,16,17,18,19,21,22,25,26,28,30,33,34,36,37,38,39,40,41,42,43,44,45,48,49,52,53,56,59,62}
-#the set of joints of the snake
-J=J4
-directions=[(1,0,0),(0,1,0),(0,0,1),(-1,0,0),(0,-1,0),(0,0,-1)]
-initialpos=[(1,0,0),(0,0,0),(1,1,0),(1,1,1)] #possible starting points (without symmetries) on a 4-cube)
-L=[initialpos[0]] #List of positions
-n=0 #length of iterated snake
-d=0 # number of direction
-D=[0] #list of directions
-j=0 #number of selected initialposition
-def in_cube(n):  #to form the cube
-    cube=set()
-    for i in range(0,n):
-        for j in range(0,n):
-            for k in range(0,n):
-                cube.add((i,j,k))
+CUBE_SIZE = 4  # length of edge of cube
+J3 = {1, 3, 5, 7, 9, 10, 11, 12, 14, 16, 17, 18, 20, 21, 23, 24, 25}
+J4 = {1, 4, 5, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 21, 22, 25, 26, 28, 30,
+      33, 34, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 48, 49, 52, 53, 56, 59, 62}
+# the set of joints of the snake
+J = J4
+directions = [(1, 0, 0), (0, 1, 0), (0, 0, 1),
+              (-1, 0, 0), (0, -1, 0), (0, 0, -1)]
+# possible starting points (without symmetries) on a 4-cube)
+initialpos = [(1, 0, 0), (0, 0, 0), (1, 1, 0), (1, 1, 1)]
+L = [initialpos[0]]  # List of positions
+n = 0  # length of iterated snake
+d = 0  # number of direction
+D = [0]  # list of directions
+j = 0  # number of selected initialposition
+
+
+def in_cube(n):  # to form the cube
+    cube = set()
+    for i in range(0, n):
+        for j in range(0, n):
+            for k in range(0, n):
+                cube.add((i, j, k))
     return cube
 
-cube= in_cube(CUBE_SIZE)
-#print cube, len(cube), J
-s=0 #number of solutions
-i=0 # number of steps
+cube = in_cube(CUBE_SIZE)
+# print cube, len(cube), J
+s = 0  # number of solutions
+i = 0  # number of steps
 start = time.clock()
 
 # sum_of: sums two list component wise
-def sum_of(a,b):
-    return (a[0]+b[0],a[1]+b[1],a[2]+b[2])
 
-#change_initialpos:
-#when all possibilities of one initialposition are checked, jump to next one
+
+def sum_of(a, b):
+    return (a[0] + b[0], a[1] + b[1], a[2] + b[2])
+
+# change_initialpos:
+# when all possibilities of one initialposition are checked, jump to next one
+
+
 def change_initialpos(j):
-    di=0
-    Di=[0]
+    di = 0
+    Di = [0]
     run_time = time.clock() - start
     print run_time
     print 'New Position'
-    if j<3:
-        j+=1
-        L= [initialpos[j]]
-        return (L,Di,di,j)
+    if j < 3:
+        j += 1
+        L = [initialpos[j]]
+        return (L, Di, di, j)
     else:
-        j+=1
+        j += 1
         print 'All checked'
-        return ([],Di,di,j)
+        return ([], Di, di, j)
 
-#change direction:
-#whenever a step fails, the direction gets changed or the snake steps back (shorten the list L)
-def change_direction(Li,Di,di,j):
-    #print 'change'
+# change direction:
+# whenever a step fails, the direction gets changed or the snake steps
+# back (shorten the list L)
+
+
+def change_direction(Li, Di, di, j):
+    # print 'change'
 
     # Should this really be D, and not Di?
-    if D !=[]:
-        while (di+1)%6 == Di[-1]:
+    if D != []:
+        while (di + 1) % 6 == Di[-1]:
             Li.pop()
-            di=Di.pop()
-            #print 'pop'
+            di = Di.pop()
+            # print 'pop'
 
-            if Di==[]:
+            if Di == []:
                 return change_initialpos(j)
 
             while len(Li) not in J:
                 Li.pop()
-                di=Di.pop()
-                #print 'pop2'
+                di = Di.pop()
+                # print 'pop2'
 
             else:
-                (Li,Di,di,j)= change_direction(Li,Di,di,j)
-            return (Li,Di,di,j)
+                (Li, Di, di, j) = change_direction(Li, Di, di, j)
+            return (Li, Di, di, j)
         else:
-            # You have random whitespace in your code.  Why is there a
-            # space before the + here?
-            di = (di +1)%6
-            return (Li,Di,di,j)
+            di = (di + 1) % 6
+            return (Li, Di, di, j)
     else:
         return change_initialpos(j)
 
 
-#steps: either adds one position to the List L (the snake), or removes one, combined with
-def steps(Li,Di,di,j): # main procedure
-    n=len(Li)
+# steps: either adds one position to the List L (the snake), or removes
+# one, combined with
+def steps(Li, Di, di, j):  # main procedure
+    n = len(Li)
 
-    p=sum_of(Li[-1], directions[di])
+    p = sum_of(Li[-1], directions[di])
 
-    if p in cube and not p in Li:# and (n<20 or (not dead_end(K,p) and connected(K,p))):
+    # and (n<20 or (not dead_end(K,p) and connected(K,p))):
+    if p in cube and not p in Li:
 
         Li.append(p)
         Di.append(di)
 
-        if (n+1) in J:
-            di= (di+1)%6
+        if (n + 1) in J:
+            di = (di + 1) % 6
     else:
         if n in J:
-            (Li,Di,di,j)=change_direction(Li,Di,di,j)
+            (Li, Di, di, j) = change_direction(Li, Di, di, j)
         else:
             #print (Li,Di,di,j)
             while len(Li) not in J:
                 Li.pop()
-                if Li==[]:
+                if Li == []:
                     return change_initialpos(j)
                 else:
                     Di.pop()
 
-            (Li,Di,di,j)=change_direction(Li,Di,di,j)
-    return (Li,Di,di,j)
-#main loop
-while n in range(0,CUBE_SIZE**3+1):
-    (L,D,d,j)= steps(L,D,d,j)
-    n=len(L)
-    i +=1
-    if i%100000==0:
+            (Li, Di, di, j) = change_direction(Li, Di, di, j)
+    return (Li, Di, di, j)
+# main loop
+while n in range(0, CUBE_SIZE**3 + 1):
+    (L, D, d, j) = steps(L, D, d, j)
+    n = len(L)
+    i += 1
+    if i % 100000 == 0:
         print(i)
         run_time = time.clock() - start
-        print run_time, run_time*1000/i
-        print (L,len(L),D,i,j)
-    if len(L)== CUBE_SIZE**3:
-        s+=1
+        print run_time, run_time * 1000 / i
+        print (L, len(L), D, i, j)
+    if len(L) == CUBE_SIZE**3:
+        s += 1
         print ('Solution!')
         print s
-        print (L,len(L),D,i,j)
+        print (L, len(L), D, i, j)
         run_time = time.clock() - start
-        print run_time, run_time*1000/i
+        print run_time, run_time * 1000 / i
 
-    if j>3:
+    if j > 3:
         print 'All possibilities checked, solutions found:'
         print s
         run_time = time.clock() - start
