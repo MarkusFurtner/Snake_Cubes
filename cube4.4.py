d12378deb44034681505edff3c8fef7df9308d2e
Mark Probst <mark.probst@gmail.com>
2016-03-02 08:31:28 -0800

    Early termination checks

--- cube4.4.py
+++ cube4.4.py
@@ -54,6 +54,43 @@ def change_initialpos(j):
         print 'All checked'
         return ([], Di, di, j)
 
+
+#pappend: a help procedure to check connectedness
+def transitive(K, start):
+    P = set(start)
+    worklist = [start]
+    for p in worklist:
+        for e in directions:
+            q = sum_of(p, e)
+            if q in K and q not in P:
+                P.add (q)
+                worklist.append (q)
+    return P
+
+#connected: checks if K is connected, if that is not the case, this path will be abandoned
+def connected(K,p):
+    if p in K:
+        return K == transitive (K, p)
+    else:
+        return False
+
+#dead_end: If K has 3 or more dead ends, the path will be abandoned
+def dead_end(K,p):
+    a=0
+    for k in K:
+        if k!=p:
+            num_empty_neighbors = 0
+            for e in directions:
+                x=sum_of(k,e)
+                if x in K:
+                    num_empty_neighbors += 1
+            if num_empty_neighbors < 2:
+                a+=1
+                if a>1:
+                    return True
+    return False
+
+
 # change direction:
 # whenever a step fails, the direction gets changed or the snake steps
 # back (shorten the list L)
@@ -92,8 +129,9 @@ def steps(Li, Di, di, j):  # main procedure
 
     p = sum_of(Li[-1], directions[di])
 
-    # and (n<20 or (not dead_end(K,p) and connected(K,p))):
-    if p in cube and not p in Li:
+    K = cube - set(Li)
+    if p in K and (n<20 or (not dead_end(K,p) and connected(K,p))):
+    #if p in cube and not p in Li:
 
         Li.append(p)
         Di.append(di)
